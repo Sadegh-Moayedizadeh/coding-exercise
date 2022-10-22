e131b481ae7222884214bc62033a4a704ab0723d
sadegh <sadegh.moayedizadeh@gmail.com>
2022-10-23 00:00:43 +0330

    level of addiction

---
+++ solutions/2887.py
@@ -0,0 +1,9 @@
+from functools import reduce
+from math import gcd
+
+
+if __name__ == '__main__':
+    number_of_numbers = int(input())
+    numbers = list(map(int, input().split(' ')))
+    level_of_addiction = reduce(gcd, numbers, numbers[0])
+    print(sum(number//level_of_addiction for number in numbers))
