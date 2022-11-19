fd9778841d1b80473369035dd8a725e23fe13188
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-20 00:24:06 +0330

    team views

--- quera/django_problems/76281/account/views.py
+++ quera/django_problems/76281/account/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.hashers import check_password
 from typing import Optional
 
-from .models import Account
-from .forms import SignUpForm, LoginForm
+from .models import Account, Team
+from .forms import SignUpForm, LoginForm, TeamForm
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -45,13 +45,44 @@ def login_account(request: HttpRequest) -> Optional[HttpResponse]:
 
 
 def logout_account(request):
-    pass
+    if request.method == 'GET':
+        logout(request)
+        return HttpResponseRedirect('/account/login/')
 
 
 @login_required
-def joinoradd_team(request):
-    pass
+def joinoradd_team(request: HttpRequest) -> HttpResponse:
+    user_account = Account.objects.get(
+        id=request.user.id
+    )
+    if request.method == 'GET':
+        if user_account.team is None:
+            return HttpResponseRedirect('/home/')
+        else:
+            return HttpResponseRedirect('/home/')
+            return render(request, 'team.html', context={'form': TeamForm()})
+    elif request.method == 'POST':
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            team_name = form.cleaned_data.get('name')
+            if Team.objects.filter(name=team_name).exists():
+                user_account.team = Team.objects.get(name=team_name)
+                user_account.save(update_fields=['team'])
+            else:
+                team = Team.objects.create(
+                    name=team_name,
+                    jitsi_url_path='http://meet.jit.si/{}'.format(team_name)
+                )
+                user_account.team = team
+                user_account.save(update_fields=['team'])
+        return HttpResponseRedirect('/home/')
 
 
 def exit_team(request):
-    pass
+    if request.method == 'GET':
+        user_account = Account.objects.get(
+            id=request.user.id
+        )
+        user_account.team = None
+        user_account.save(update_fields=['team'])
+        return HttpResponseRedirect('/home/')
