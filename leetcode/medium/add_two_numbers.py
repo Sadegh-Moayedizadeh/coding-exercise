a24ad8917799963cf79e40086ff579c2fb6eeb7a
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-23 00:19:43 +0330

    add two numbers

---
+++ leetcode/medium/add_two_numbers.py
@@ -0,0 +1,32 @@
+import pytest
+from typing import List
+
+
+class Solution:
+    def addTwoNumbers(
+        self,
+        l1: List[int],
+        l2: List[int]
+    ) -> List[int]:
+        first_number = self._convert_list_to_number(l1)
+        second_number = self._convert_list_to_number(l2)
+        return self._convert_number_into_list(first_number + second_number)
+
+    def _convert_list_to_number(self, lst: List[int]) -> int:
+        return int(''.join(map(str, lst))[::-1])
+
+    def _convert_number_into_list(self, number) -> List[int]:
+        return list(map(int, str(number)))[::-1]
+
+
+@pytest.mark.parametrize('l1, l2, expected_result', [
+    ([2, 4, 3], [5, 6, 4], [7, 0, 8]),
+    ([0], [0], [0]),
+    ([9,9,9,9,9,9,9], [9,9,9,9], [8,9,9,9,0,0,0,1])
+])
+def test(l1: List[int], l2: List[int], expected_result: List[int]) -> None:
+    # Arrange, Act
+    actual_result = Solution().addTwoNumbers(l1, l2)
+
+    # Assert
+    assert actual_result == expected_result
