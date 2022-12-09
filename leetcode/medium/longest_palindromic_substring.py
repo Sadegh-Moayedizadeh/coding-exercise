19f696a25f3f220a08fe2424afae0e341bf59b97
sadegh <sadegh.moayedizadeh@gmail.com>
2022-12-09 20:00:47 +0330

    faster solution

--- leetcode/medium/longest_palindromic_substring.py
+++ leetcode/medium/longest_palindromic_substring.py
@@ -1,20 +1,29 @@
 class Solution:
-    _cache = {}
-
     def longestPalindrome(self, s: str) -> str:
-        if s in self._cache:
-            return self._cache[s]
-        i, j = 0, len(s) - 1
-        while i < j:
-            if s[i] != s[j]:
-                answer = max(
-                    self.longestPalindrome(s[0: len(s)-1]),
-                    self.longestPalindrome(s[1: len(s)]),
-                    key=len
-                )
-                self._cache[s] = answer
-                return answer
-            i += 1
-            j -= 1
-        self._cache[s] = s
-        return s
+        s1 = s
+        s2 = s[::-1]
+        result = ''
+        for d in range(1, len(s1) + len(s2)):
+            i1 = self._positive_or_zero(len(s1) - d)
+            j1 = len(s1) - self._positive_or_zero(d - len(s2))
+            i2 = self._positive_or_zero(d - len(s1))
+            j2 = len(s2) - self._positive_or_zero(len(s2) - d)
+            sub1 = s1[i1: j1]
+            sub2 = s2[i2: j2]
+            
+            new_result = ''
+            i = 0
+            for j in range(1, len(sub1) + 1):
+                if sub1[i: j] == sub2[i: j]:
+                    if len(sub1[i: j]) > len(new_result):
+                        new_result = sub1[i: j]
+                    continue
+                else:
+                    i = j
+            if len(new_result) > len(result):
+                result = new_result
+
+        return result
+
+    def _positive_or_zero(self, n: int) -> int:
+        return 0 if n < 0 else n
