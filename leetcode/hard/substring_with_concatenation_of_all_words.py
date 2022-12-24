0871696bcfa78cda1399882216fd19632ca55185
sadegh <sadegh.moayedizadeh@gmail.com>
2022-12-25 00:30:01 +0330

    substring with concatenation of all words.

---
+++ leetcode/hard/substring_with_concatenation_of_all_words.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        result = []
+        i = 0
+        while i < len(s):
+            if self._is_concatenation_of_all_words(
+                    s[i: i + len(words) * len(words[0])], words):
+                result.append(i)
+            i += 1
+        return result
+
+    def _is_concatenation_of_all_words(
+        self, string: str, words: List[str]
+    ) -> bool:
+        word_len = len(words[0])
+        words = Counter(words)
+        i = 0
+        while i < len(string):
+            candidate = string[i: i + word_len]
+            if candidate in words and words[candidate] > 0:
+                words[candidate] -= 1
+                i += word_len
+            else:
+                return False
+        if all(val == 0 for val in words.values()):
+            return True
+        return False
+
+
+print(Solution().findSubstring('barfoothefoobarman', ['foo','bar'])) # [0, 9]
+print(Solution().findSubstring(
+    'wordgoodgoodgoodbestword', ["word","good","best","word"]))  # []
+print(Solution().findSubstring(
+    'barfoofoobarthefoobarman', ["bar","foo","the"]))  # [6, 9, 12]
