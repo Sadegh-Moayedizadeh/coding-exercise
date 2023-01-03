24bc6a9b4707a2d063f138d76c5cdab188c4d4c9
sadegh <sadegh.moayedizadeh@gmail.com>
2023-01-04 01:02:44 +0330

    Final acceptable solution with o(n).

--- leetcode/hard/longest_valid_parantheses.py
+++ leetcode/hard/longest_valid_parantheses.py
@@ -1,28 +1,36 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        result = 0
-        i = 0
-        j = 0
-        score = 0
-        while i < len(s):
-            if j == len(s):
-                i += 1
-                j = i
-                score = 0
-                continue
+        return max(
+            self._find_max_len(s),
+            self._find_max_len(self._reverse_parantheses(s))
+        )
 
+    def _reverse_parantheses(self, s: str) -> str:
+        lst = []
+        for char in s[::-1]:
+            if char == ')':
+                lst.append('(')
+            else:
+                lst.append(')')
+        return ''.join(lst)
+
+    def _find_max_len(self, s: str) -> int:
+        result = 0
+        score = 0
+        i, j = 0, 0
+        while j < len(s):
             if s[j] == '(':
                 score += 1
             else:
                 score -= 1
 
-            if score < 0:
-                i += 1
-                j = i
-                score = 0
-            elif score == 0:
+            if score == 0:
                 result = max(result, j - i + 1)
                 j += 1
+            elif score < 0:
+                score = 0
+                j += 1
+                i = j
             else:
                 j += 1
         return result
@@ -31,3 +39,4 @@ class Solution:
 print(Solution().longestValidParentheses(')()())'))  # 4
 print(Solution().longestValidParentheses('(()'))  # 2
 print(Solution().longestValidParentheses(''))  # 0
+print(Solution().longestValidParentheses('()(()'))  # 2
