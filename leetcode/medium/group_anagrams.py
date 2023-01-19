e46db0bd5fd798de088b160d06fc34be5cd2da8c
sadegh <sadegh.moayedizadeh@gmail.com>
2023-01-19 17:15:47 +0330

    eficient solution

--- leetcode/medium/group_anagrams.py
+++ leetcode/medium/group_anagrams.py
@@ -1,25 +1,13 @@
 from typing import List
-from collections import Counter
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
-        for i in range(len(strs)):
-            if any(i in group for group in result):
-                continue
-            new_group = set()
-            new_group.add(i)
-            for j in range(i + 1, len(strs)):
-                if any(j in group for group in result):
-                    continue
-                if Counter(strs[i]) == Counter(strs[j]):
-                    new_group.add(j)
-            result.append(new_group)
-        return list(map(
-            list,
-            (map(lambda i: strs[i], group) for group in result)
-        ))
+        result_dict = defaultdict(list)
+        for string in strs:
+            result_dict[''.join(sorted(string))].append(string)
+        return list(result_dict.values())
 
 
 print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
