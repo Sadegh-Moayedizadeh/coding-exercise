cf1e2a690773fcbd905565803fdbd1d97f13c150
sadegh <sadegh.moayedizadeh@gmail.com>
2023-01-21 16:22:51 +0330

    Solution with time limit

---
+++ leetcode/medium/jump_game.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        _cache = {}
+        def cache(func):
+            def wrap(i):
+                if i in _cache:
+                    return _cache[i]
+                res = func(i)
+                _cache[i] = res
+                return res
+            return wrap
+
+        @cache
+        def dp(index) -> bool:
+            if index == len(nums) - 1:
+                return True
+            if index > len(nums) - 1:
+                return False
+            return any(dp(index + j + 1) for j in range(nums[index]))
+        return dp(0)
