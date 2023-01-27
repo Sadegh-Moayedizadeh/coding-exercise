0dee26ccc077dcb91cd26fc7c4a7358396bb5531
sadegh <sadegh.moayedizadeh@gmail.com>
2023-01-27 23:10:09 +0330

    Original suboptimal dfs solution

--- leetcode/hard/permutation_sequence.py
+++ leetcode/hard/permutation_sequence.py
@@ -1,9 +1,19 @@
-from itertools import permutations
+from typing import List
 
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        return ''.join(map(str, list(permutations(range(1, n+1), n))[k-1]))
+        result = []
+        def dfs(result_so_far: List[int]) -> List[List[int]]:
+            if len(result_so_far) == n:
+                result.append(result_so_far)
+                return
+            result_set = set(result_so_far)
+            for i in range(1, n+1):
+                if i not in result_set:
+                    dfs(result_so_far + [i])
+        dfs([])
+        return ''.join(map(str, result[k-1]))
 
 
 print(Solution().getPermutation(3, 3))  # 213
